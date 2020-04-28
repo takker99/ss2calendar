061c103c90b6e6ae5a48190072b2a1511545ae61
takker99 <electronic.homuhomu@gmail.com>
2020-04-29 08:55:07 +0900

    :bug: １行を1recordとして予定を書き出す。
    所要時間はうまく計算できていないので、一旦コメントアウトした

--- src/index.ts
+++ src/index.ts
@@ -64,21 +64,21 @@ function writeCalendar(date: Date): void
     for (let index = 1; index <= holidays.length; index++)
     {
         const holiday = holidays[index - 1];
-        sheet.getRange(1, index).setValue(holiday.getTitle()); //イベントタイトル
+        sheet.getRange(index, 1).setValue(holiday.getTitle()); //イベントタイトル
         //イベント開始時刻
-        sheet.getRange(2, index).setValue(holiday.getStartTime().getFullYear());
-        sheet.getRange(3, index).setValue(holiday.getStartTime().getMonth());
-        sheet.getRange(4, index).setValue(holiday.getStartTime().getDate());
-        sheet.getRange(5, index).setValue(holiday.getStartTime().getHours());
-        sheet.getRange(6, index).setValue(holiday.getStartTime().getMinutes());
+        sheet.getRange(index, 2).setValue(holiday.getStartTime().getFullYear());
+        sheet.getRange(index, 3).setValue(holiday.getStartTime().getMonth());
+        sheet.getRange(index, 4).setValue(holiday.getStartTime().getDate());
+        sheet.getRange(index, 5).setValue(holiday.getStartTime().getHours());
+        sheet.getRange(index, 6).setValue(holiday.getStartTime().getMinutes());
         //イベント終了時刻
-        sheet.getRange(6, index).setValue(holiday.getEndTime().getFullYear());
-        sheet.getRange(7, index).setValue(holiday.getEndTime().getMonth());
-        sheet.getRange(8, index).setValue(holiday.getEndTime().getDate());
-        sheet.getRange(9, index).setValue(holiday.getEndTime().getHours());
-        sheet.getRange(10, index).setValue(holiday.getEndTime().getMinutes());
+        sheet.getRange(index, 6).setValue(holiday.getEndTime().getFullYear());
+        sheet.getRange(index, 7).setValue(holiday.getEndTime().getMonth());
+        sheet.getRange(index, 8).setValue(holiday.getEndTime().getDate());
+        sheet.getRange(index, 9).setValue(holiday.getEndTime().getHours());
+        sheet.getRange(index, 10).setValue(holiday.getEndTime().getMinutes());
         //所要時間
-        sheet.getRange(11, index).setValue('=round((rc[-1]-rc[-2])*24*60,0)');
+        /* sheet.getRange(index, 11).setValue('=round((rc[-1]-rc[-2])*24*60,0)'); */
     }
 
     // Calendarに書き込む
