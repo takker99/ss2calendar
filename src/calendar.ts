91ad933d68a10bcb974615f5972623ac4710a849
takker99 <electronic.homuhomu@gmail.com>
2020-06-02 13:12:30 +0900

    :sparkles: :heavy_plus_sign: Use Moment.js instead of Data
    - 内部の日時形式をMoment.jsに変更
    - いらない函数その他を削除

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,64 +1,26 @@
-/* import * as moment from 'moment'; */
-/* const Moment = { moment: moment }; // GAS対策 cf. https://qiita.com/awa2/items/d24df6abd5fd5e4ca3d9 */
-
-export class Minutes {
-    constructor(private minutes: number) {
-        this.minutes = minutes;
-    }
-
-    /**
-     * 時間の長さをmilisecond単位で取得する
-     *
-     * @return 時間の長さ(miliseconds)
-     */
-    public getTime(): number {
-        return this.minutes * 60000;
-    }
-}
-
-function add(date: Date, minutes: Minutes): Date {
-    const temp: number = date.getTime() + minutes.getTime();
-    return new Date(temp);
-}
+import * as moment from 'moment';
+const Moment = { moment: moment }; // GAS対策 cf. https://qiita.com/awa2/items/d24df6abd5fd5e4ca3d9
 
 export class TimeSpan {
-    constructor(begin: Date, length: Minutes);
-    constructor(begin: Date, end: Date);
+    constructor(begin: moment.Moment, end: moment.Moment);
     constructor(begin: number, end: number);
 
-    constructor(begin: Date | number, value: Minutes | Date | number) {
-        if (begin instanceof Date) {
-            this._start = new Date(begin.getTime());
-        } else {
-            this._start = new Date(begin);
-        }
-        if (value instanceof Date) {
-            this._end = new Date(value.getTime());
-        }
-        if (value instanceof Minutes) {
-            this._end = add(this._start, value);
-        } else {
-            this._end = new Date(value);
-        }
+    constructor(begin: moment.Moment | number, end: moment.Moment | number) {
+        this._start = Moment.moment(begin);
+        this._end = Moment.moment(end);
     }
 
-    public AddMonth(months: number): void {
-        this._end.setMonth(this._end.getMonth() + months);
+    public get start(): moment.Moment {
+        return Moment.moment(this._start);
     }
 
-    public get start(): Date {
-        return new Date(this._start.getTime());
+    public get end(): moment.Moment {
+        return Moment.moment(this._end);
     }
 
-    public get end(): Date {
-        return new Date(this._end.getTime());
-    }
-
-    private _start: Date;
-    private _end: Date;
+    private _start: moment.Moment;
+    private _end: moment.Moment;
 }
-
-type CalendarEvent = GoogleAppsScript.Calendar.CalendarEvent;
 
 export class Calendar {
     constructor(calendarId: string) {
@@ -69,10 +31,10 @@ export class Calendar {
         const event = this.calendar.getEventById(eventId);
         if (event.getTitle() != newEvent.title) event.setTitle(newEvent.title);
         if (
-            event.getStartTime() != newEvent.start &&
-            event.getEndTime() != newEvent.end
+            event.getStartTime() != newEvent.start.toDate() &&
+            event.getEndTime() != newEvent.end.toDate()
         )
-            event.setTime(newEvent.start, newEvent.end);
+            event.setTime(newEvent.start.toDate(), newEvent.end.toDate());
         if (event.getDescription() != newEvent.option.description)
             event.setDescription(newEvent.option.description);
     }
@@ -80,8 +42,8 @@ export class Calendar {
     public Add(event: Event): string {
         const result = this.calendar.createEvent(
             event.title,
-            event.start,
-            event.end,
+            event.start.toDate(),
+            event.end.toDate(),
             event.option
         );
         return result.getId();
@@ -109,11 +71,11 @@ export class Event {
         return this._title;
     }
 
-    public get start(): Date {
+    public get start(): moment.Moment {
         return this.period.start;
     }
 
-    public get end(): Date {
+    public get end(): moment.Moment {
         return this.period.end;
     }
 
@@ -135,6 +97,7 @@ export function getDateFixed(
     day: number,
     hour: number,
     minute: number
-): Date {
-    return new Date(year, month - 1, day, hour, minute);
+): moment.Moment {
+    return Moment.moment({year: year, month: month - 1, day: day, hour: hour, minute: minute});
+
 }
