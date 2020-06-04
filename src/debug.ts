bcfab6727562c2c41e4ff7b7b508f518793c80c4
takker99 <electronic.homuhomu@gmail.com>
2020-06-04 15:18:29 +0900

    :sparkles: :alembic: Logged the values in an onEdit event object when any user edit the spread sheet

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,24 @@
 import * as moment from 'moment';
+import { OnEditEventObject } from './EventObject';
 const Moment = { moment: moment };
+
+// userが編集したときに発火する函数
+// scriptの編集には反応しないので注意
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function onEdit(e: OnEditEventObject): void {
+    console.log(`AuthMode: ${e.authMode}`);
+    console.log(`Changed range: ${e.range}`);
+    console.log(`User: ${e.user}`);
+    console.log(`Changed spread sheet: ${e.source}`);
+    if (e.oldValue) {
+        console.log(`s/${e.oldValue}/${e.value}`);
+    } else {
+        console.log('More than one cells are changed');
+    }
+    console.log(
+        `Trigger ID: ${e.triggerUid ? e.triggerUid : 'simple trigger'}`
+    );
+}
 
 // 機能テスト用のscript
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
